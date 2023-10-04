a6a8450c0f1b2c312f625e0cad3199e46e1d9b8a
Shin <ssh2375@naver.com>
2023-10-04 14:46:06 +0900

    week5.2 차원 배열 (범위 기반 배열)

--- challenge/week5/randomMax.cpp
+++ challenge/week5/randomMax.cpp
@@ -12,31 +12,28 @@ int main() {
 			numlist[i][j] = elem;
 			cout << i << "," << j << ":" << elem << endl;
 		}
-	//i를 변수 선언 및 초기화 진행 후 0일 때 j가 0부터 9까지 반복되면서 0,1  0,2 .... 반복 되고 
-	//그에 해당하는 numlist 값에 elem 값을 넣어주는 데 elem은 랜덤으로 생성
-	//위의 과정을 i가 0부터 9가지 반복
+		//i를 변수 선언 및 초기화 진행 후 0일 때 j가 0부터 9까지 반복되면서 0,1  0,2 .... 반복 되고 
+		//그에 해당하는 numlist 값에 elem 값을 넣어주는 데 elem은 랜덤으로 생성
+		//위의 과정을 i가 0부터 9가지 반복
 	}
 	cout << endl;
 
 	int max = 0;
 	int maxI = 0;
 	int maxJ = 0;
-	// 변수 선언
+	// 변수 선언 및 초기화
 	for (int i = 0; i < numCell; i++) {
-		for (int j = 0; j < numCell; j++) {
-			int value = numlist[i][j];
-			// 배열 0,0  0,1 .... 이렇게 반복하면서 그에 해당하는 값을 value 값에 넣어준다.
-			if (value > max) {
-				max = value;
-				maxI = i;
-				maxJ = j;
-			}
-			// 그 값을 max 와 비교하여 크면 해당하는 값과 i,j를 위에서 선언한 변수에 대입
+		int j = 0; // 변수 선언 및 for 구문 작성
+		for (auto value: numlist[i]){ //범위 기반 배열 준비 $$
+			// numlist[i]에 해당하는 배열의 한칸을 value에 넣어준다
+			if (max < value);
+			max = value;
+			maxI = i;
+			maxJ = j;
 		}
-		// 위 과정을 0,0 ~ 9,9까지 반복하여 max를 찾는다.
-	}
-
-	//안쪽 for 문을 범위기반으로도 생각해보자!
+		j++; //j가 한칸한칸씩 증가하도록 해줌
+		
+	}// 위 과정을 0,0 ~ 9,9까지 반복하여 max를 찾는다.
 
 	cout << "가장 큰 값은 " << max << "이고,";
 	cout << "i와 j는 각각" << maxI << "," << maxJ << "입니다." << endl;
